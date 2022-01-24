b0b361cc370a6995147bdd44722abdd3f526b6f3
HinanJiHyun <wlgusdldi15@naver.com>
2022-01-24 16:40:51 +0900

    components basic

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <MyBtn>Banana</MyBtn>
+  <MyBtn color="#f90">
+    Apple
+  </MyBtn>
+  <MyBtn :color="color">
+    <span style="color: red">cherry</span>
+  </MyBtn>
+  <MyBtn
+    large
+    color="#6e0">
+    Orange
+  </MyBtn>
+  <MyBtn>Banana</MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default {
+  components: {
+    MyBtn
+  },
+  data(){
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
